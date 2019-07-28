6ca2b22aeac775ba36a22431d799348bdcf65912
ken-kentan <ken.kentan.k2@gmail.com>
2019-07-28 21:23:19 +0900

    add MyPackageInstaller

--- app/src/main/java/jp/kentan/dev/customappstore/MainActivity.java
+++ app/src/main/java/jp/kentan/dev/customappstore/MainActivity.java
@@ -2,9 +2,12 @@ package jp.kentan.dev.customappstore;
 
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
+import android.content.pm.PackageInstaller;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDevicePolicyManager = (DevicePolicyManager)getSystemService(Context.DEVICE_POLICY_SERVICE);
+        final PackageInstaller packageInstaller = getPackageManager().getPackageInstaller();
 
         mTextView = findViewById(R.id.text_view);
 
@@ -25,6 +29,27 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.install_button).setOnClickListener((view) -> {
             mTextView.setText("Install clicked.");
+
+            new Thread(new Runnable(){
+                public void run() {
+                    try {
+                        URL url = new URL("https://example.com/example.apk");
+
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setAllowUserInteraction(false);
+                        conn.setInstanceFollowRedirects(true);
+                        conn.setRequestMethod("GET");
+                        conn.connect();
+
+                        MyPackageInstaller.installPackage(getApplicationContext(), "com.example", conn.getInputStream());
+
+//                        boolean success = installPackage(getApplicationContext(), conn.getInputStream(), "gtav");
+//                        Log.d("INSTALL", "" + success);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         });
 
         findViewById(R.id.uninstall_button).setOnClickListener((view) -> {
---
+++ app/src/main/java/jp/kentan/dev/customappstore/MyPackageInstaller.java
@@ -0,0 +1,50 @@
+package jp.kentan.dev.customappstore;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInstaller;
+import android.util.Log;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class MyPackageInstaller {
+
+    static void installPackage(Context context, String packageName, InputStream apkStream) throws Exception {
+        PackageInstaller packageInstaller = context.getPackageManager().getPackageInstaller();
+
+
+        PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(PackageInstaller.SessionParams.MODE_FULL_INSTALL);
+        params.setAppPackageName(packageName);
+
+        int sessionId = packageInstaller.createSession(params);
+        PackageInstaller.Session session = packageInstaller.openSession(sessionId);
+
+        Log.d("MyPackageInstaller", "open session: " + sessionId);
+
+        OutputStream out = session.openWrite(packageName, 0, -1);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        int count = 0;
+        while ((length = apkStream.read(buffer)) != -1) {
+            out.write(buffer, 0, length);
+            count += length;
+        }
+
+        Log.d("MyPackageInstaller", "write: " + count);
+
+        session.fsync(out);
+        out.close();
+        apkStream.close();
+
+        session.commit(PendingIntent.getBroadcast(context, sessionId,
+                new Intent("android.intent.action.MAIN"), 0).getIntentSender());
+
+        Log.d("MyPackageInstaller", "session commit");
+
+        session.close();
+
+        Log.d("MyPackageInstaller", "session close");
+    }
+}
